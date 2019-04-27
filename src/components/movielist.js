963a3b24db5191b7d13c460756adb990d13a8b64
Mark <marknilov@yahoo.com>
2019-04-27 08:10:51 +0100

    Start with create-react-app at git

--- src/components/movielist.js
+++ src/components/movielist.js
@@ -51,6 +51,7 @@ class MovieList extends Component {
             <Carousel.Caption>
             <h3>{movie.title}</h3>
             <Glyphicon glyph={'star'} /> {"5"} &nbsp;&nbsp; {movie.year}
+            <h3>{movie.year}</h3>
             </Carousel.Caption>
             </Carousel.Item>)}
             </Carousel>);
